90bd2e8a02f4921740b43de514fcca22457f8fef
Ryan Li <ryan.lichangrui@gmail.com>
2019-12-16 21:59:15 -0500

    fixed typos, changed css

--- js/contributors.js
+++ js/contributors.js
@@ -1,16 +1,17 @@
 async function getContributors(){
     let url = "https://api.github.com/repos/RyanL123/Chemistry-11/contributors"
-    // let url = "https://api.github.com/repos/michalsnik/aos/contributors";
     let response = await fetch(url);
     let contributors = await response.json();
     let contributorsDiv = document.getElementById("contributors");
-    // console.log(result);
     for (let i = 0; i < contributors.length; i++){
         contributorsDiv.innerHTML += "<div class=\"contributors-sub-div\">"
-        + "<div class=\"contributors-img-div\">"
+        + "<div class=\"contributors-img-div\">" 
+        + "<a target=\"_blank\" href=\""
+        + contributors[i].html_url
+        + "\">"
         + "<img src=\"" + contributors[i].avatar_url + "\">" 
-        + "</div>"
-        + "<a href=\"" + contributors[i].html_url + "\"><h2 class=\"contributor-name\">"
+        + "</a></div>"
+        + "<a target=\"_blank\" href=\"" + contributors[i].html_url + "\"><h2 class=\"contributor-name\">"
         + contributors[i].login
         + "</h2></a>"
         + "</div>";
