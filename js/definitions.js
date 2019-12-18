a3cb25838b2410f7c5deb3d7a2c237ee57f42274
Ryan Li <ryan.lichangrui@gmail.com>
2019-12-18 17:48:22 -0500

    added solubility rules, changed filter to prioritize search terms

--- js/definitions.js
+++ js/definitions.js
@@ -13,45 +13,60 @@ xhr.onreadystatechange = function() {
             function(a,b){
                 return a.Term.localeCompare(b.Term);
             }
-        );  
+        );
         
         //Write elements in order, formatting them as hyperlinks
-        for (var i = 0; i < definitions.definitions.length; i++){
-            document.getElementById("definitions-div").innerHTML += "<div class=\"definitions\"><h2>" + "<a href=" + definitions.definitions[i].Link + " target=\"_blank\">" + definitions.definitions[i].Term + "</a></h2><p>" + definitions.definitions[i].Definition + "</p></div>"
-        }
+        writeCards(document.getElementById("definitions-cards-container"), definitions.definitions);
     }
 }
 xhr.send(null);
 
 
-//Filters definitions based on whats in the search bar
-function filter(){
-    //Get objects
-    var input = document.getElementById("filter");
-    var filter = input.value.toUpperCase();
-    var container = document.getElementById("definitions-div");
-    var definitions = container.getElementsByClassName("definitions");
+//filters cards, prioritizing definitions with the search query in the title
+function filter() {
+    // parses definitions.json into definitions variable
+    let url = "../JSON/definitions.json"
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var definitions = JSON.parse(xhr.responseText);
 
-    //Initially hide all elements
-    for (var i = 0; i < definitions.length; i++){
-        var title = definitions[i].querySelector(".definitions h2");
-        var content = definitions[i].querySelector(".definitions p")
-
-        definitions[i].style.display = "none";
-    }
-
-    //Iterate through definitions
-    for (var i = 0; i < definitions.length; i++){
-        var title = definitions[i].querySelector(".definitions h2");
-        var content = definitions[i].querySelector(".definitions p")
-
-        //Only show if content in filter matches content in definitions
-        if (title.innerText.toUpperCase().indexOf(filter) > -1 || content.innerText.toUpperCase().indexOf(filter) > -1){
-            definitions[i].style.display = "";
+            // For some reason definitons cannot be modified on the global scope,
+            // so everything has to be done within the same scope as JSON.parse request
+            // Sort array elements lexicographically by their term
+            definitions.definitions.sort(
+                function(a,b){
+                    return a.Term.localeCompare(b.Term);
+                }
+            );
+            
+            //Write elements in order, formatting them as hyperlinks
+            let filter = document.getElementById("filter").value;
+            let filteredResultsTitle = [];
+            let filteredResultsContent = [];
+            for (let i = 0; i < definitions.definitions.length; i++){
+                // Add definition to array if there is a match
+                if (definitions.definitions[i].Term.toUpperCase().indexOf(filter.toUpperCase()) > -1){
+                    filteredResultsTitle.push(definitions.definitions[i]);
+                }
+                else if (definitions.definitions[i].Definition.toUpperCase().indexOf(filter.toUpperCase()) > -1){
+                    filteredResultsContent.push(definitions.definitions[i]);
+                }
+            }
+            // Write cards by concatonating the arrays, with results title being first
+            writeCards(document.getElementById("definitions-cards-container"), filteredResultsTitle.concat(filteredResultsContent));
         }
     }
+    xhr.send(null);
+}
 
-    // var t = "definitions-div".textContent || "definitions-div".innerTExt;
-    // container.innerHTML = container.value ? t.replace(new RegExp('('+conatiner.value+')','ig'),
-    // '<span class=\"highlight\">$1</span>') : t;
+function writeCards(container, cardsArray){
+    container.innerHTML = "";
+    for (let i = 0; i < cardsArray.length; i++){
+        container.innerHTML += "<div class=\"card\"><h2 class=\"subtitle\">" + 
+        "<a href=" + cardsArray[i].Link + 
+        " target=\"_blank\">" + cardsArray[i].Term + 
+        "</a></h2><h3>" + cardsArray[i].Definition + "</h3></div>"
+    }
 }
