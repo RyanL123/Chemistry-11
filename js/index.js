8901ef785dcc5d98b0a37cfefac084f70ecf0aa0
Ryan <ryan.lichangrui@gmail.com>
2020-01-02 16:58:18 -0500

    changed website stats banner, added desktop mode snackbar on mobile

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-// $(function() {
-//   var siteFeaturesDistance = $("#site-features").offset().top-100,
-//   $window = $(window)
-//   var mainContentDistance = $("#main-content").offset().top-100,
-//   $window = $(window)
-//     $(window).scroll(function () {
-//        if ($(this).scrollTop() > siteFeaturesDistance) {
-//           $('body').addClass('changeColor')
-//        }
-//        if ($(this).scrollTop() < siteFeaturesDistance || $(this).scrollTop() >= mainContentDistance) {
-//           $('body').removeClass('changeColor')
-//        }
-//     });
-//  });
-
-
 // Firebase config
 var config = {
   apiKey: "AIzaSyD-KQ_P6gYE7ZVr2NyojK70iWXs8lhXwzc",
@@ -30,23 +14,12 @@ firebase.analytics();
 
 // Smooth scrolling
 function smoothScroll(){
-  window.scroll({
-    top: 2500, 
-    left: 0, 
-    behavior: 'smooth'
-  });
-    
-  // Scroll certain amounts from current position 
-  window.scrollBy({ 
-    top: 100, // could be negative value
-    left: 0, 
-    behavior: 'smooth' 
-  });
-    
-  // Scroll to a certain element
-  document.getElementById('site-features').scrollIntoView({ 
-    behavior: 'smooth',
-    block: "start",
-    inline: "center"
-  });
+  window.scrollTo({
+    top: 900,
+    behavior: "smooth"
+  })
+}
+
+function hideDesktopBar() {
+  document.getElementById("desktop-bar").style.display = "none";
 }
