52d8e84c190a21c31a52a05cd66b8443c6759bbc
Ryan <ryan.lichangrui@gmail.com>
2020-08-02 13:20:09 -0400

    add alt attribute to contributor images

--- js/contributors.js
+++ js/contributors.js
@@ -1,20 +1,28 @@
-async function getContributors(){
-    let url = "https://api.github.com/repos/RyanL123/Chemistry-11/contributors"
+async function getContributors() {
+    let url = "https://api.github.com/repos/RyanL123/Chemistry-11/contributors";
     let response = await fetch(url);
     let contributors = await response.json();
     let contributorsDiv = document.getElementById("contributors");
-    for (let i = 0; i < contributors.length; i++){
-        contributorsDiv.innerHTML += "<div class=\"contributors-sub-div\">"
-        + "<div class=\"contributors-img-div\">" 
-        + "<a target=\"_blank\" href=\""
-        + contributors[i].html_url
-        + "\">"
-        + "<img src=\"" + contributors[i].avatar_url + "\">" 
-        + "</a></div>"
-        + "<a target=\"_blank\" href=\"" + contributors[i].html_url + "\"><h2 class=\"contributor-name\">"
-        + contributors[i].login
-        + "</h2></a>"
-        + "</div>";
+    for (let i = 0; i < contributors.length; i++) {
+        contributorsDiv.innerHTML +=
+            '<div class="contributors-sub-div">' +
+            '<div class="contributors-img-div">' +
+            '<a target="_blank" href="' +
+            contributors[i].html_url +
+            '">' +
+            '<img src="' +
+            contributors[i].avatar_url +
+            '"' +
+            'alt="' +
+            contributors[i].login +
+            '">' +
+            "</a></div>" +
+            '<a target="_blank" href="' +
+            contributors[i].html_url +
+            '"><h2 class="contributor-name">' +
+            contributors[i].login +
+            "</h2></a>" +
+            "</div>";
     }
 }
 
